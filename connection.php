dbdf3cdb0b4cccebe0004df11c153280dc10ab05
B'BEAM <yinglae@gmail.com>
2020-04-29 03:15:07 +0700

    test db v2

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 //?>
 <?php
 $con=mysqli_connect("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ra4djws574rbluuq","b1ks82sowy27q7nr","nli1p003te1rfojq") or die(mysqli_error($con));
